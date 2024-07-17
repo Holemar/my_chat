bcc7618a42a70a8a21c6c3e270c35043f463cfdd
holemar <daillow@gmail.com>
2024-07-17 16:46:59 +0800

    csv_xlsx自启动

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-ps aux|grep my_chat.py | grep -v grep|awk '{print$2}'|xargs kill -9
-nohup python3 my_chat.py > output.log 2>&1 &
+# ps aux|grep my_chat.py | grep -v grep|awk '{print$2}'|xargs kill -9
+# nohup python3 my_chat.py > output.log 2>&1 &
+
+export PORT=18080
+ps aux|grep csv_xlsx.py | grep -v grep|awk '{print$2}'|xargs kill -9
+nohup python3 csv_xlsx.py > output.log 2>&1 &
+
